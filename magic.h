068314c2720777323aa608330bed6f6abcd81260
wweidler <weidlerwill@gmail.com>
2022-04-25 22:01:03 -0500

    Code works as intended and notation completed

--- magic.h
+++ magic.h
@@ -4,6 +4,17 @@
 #include <iostream>
 using namespace std;
 
+/**************FUNCTION NOTATION**************/
+// Function: MagicSmasher(T val1, T val2);
+// Description: A constructor that assigns m_val1 to val1 and m_val2 to val2
+// Pre: Two values of the same type
+// Post: A fully initialized MagicSmasher
+
+// Function: T getSum();
+// Description: Returns the sum of the two values
+// Pre: An initialized MagicSmasher
+// Post: Returns a value of type T corresponding to the sum of the two values
+
 /**************CLASS DEFINITION**************/
 template <class T>
 class MagicSmasher{
@@ -15,7 +26,7 @@ class MagicSmasher{
     private:
         T m_val1;
         T m_val2;
-        T m_sum;
 };
 
+#include "magic.hpp"
 #endif
